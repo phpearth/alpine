9f9ac87a76358cd8eb0d666beb5f18a920b1dfaa
Elan Ruusamäe <glen@delfi.ee>
2019-05-26 01:26:24 +0300

    use $(MAKE) variable
    thus when invoked via gmake, it would use same program, not fallback to make again

--- .make.mk
+++ .make.mk
@@ -13,28 +13,28 @@ build: ## Build necessary Docker image for building packages
   @docker image build -t phpearth-abuild:$(version) -f .docker/abuild/Dockerfile .docker/abuild
 
 run: ## Run a command in a new Docker container; make run a=[...]
-  make build
+  $(MAKE) build
   @docker container run -it --rm -v `pwd`/packages:/$(version)/ -v `pwd`/public:/public phpearth-abuild:$(version) $(a)
 
 package: ## Usage: make package [p="7.1|7.2|7.3|all|<package-name1> <package-name2> ..."]
   @test "$(p)"
-  make run a="package -p \"$(p)\""
+  $(MAKE) run a="package -p \"$(p)\""
 
 generate-index: ## Generate index file APKINDEX.tar.gz usage: make generate-index
-  make run a="generate-index"
+  $(MAKE) run a="generate-index"
 
 private-key: ## Generate new private key
-  make run a="openssl genrsa -out phpearth.rsa.priv 4096 --build --force-recreate"
+  $(MAKE) run a="openssl genrsa -out phpearth.rsa.priv 4096 --build --force-recreate"
   mv packages/phpearth.rsa.priv .docker/abuild/home/packager/.abuild/
 
 public-key: ## Generate new public key
-  make run a="openssl rsa -in /home/packager/.abuild/phpearth.rsa.priv -pubout -out /public/phpearth.rsa.pub"
+  $(MAKE) run a="openssl rsa -in /home/packager/.abuild/phpearth.rsa.priv -pubout -out /public/phpearth.rsa.pub"
 
 clean: ## Remove pkg, src, and tmp folders when building packages for Alpine
   @rm -rf packages/*/pkg packages/*/src packages/*/tmp
 
 sh: ## Run shell
-  make run a=sh
+  $(MAKE) run a=sh
 
 upload: ## Upload build packages to Linux repos server
   @rsync -avz --del public/$(version)/ repos.php.earth:~/repos/alpine/$(version)/
